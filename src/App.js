84fcf275e2f5094d26e64656c2d5e22835a8b764
Ivan <ivanteozhiming@gmail.com>
2021-07-28 21:17:52 +0800

    remove duplicates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // import logo from './logo.svg';
-import "./App.css";
 import NavBar from "./components/layout/NavBar";
 // import LandingPage from "./pages/LandingPage";
 import { useRef } from "react";
